1b26d74e7356cb1161f63686f1797295145cd25e
kkaris <karis.klas@gmail.com>
2023-03-28 16:17:03 -0700

    Remove commented code

--- check_correctness/check_correctness.py
+++ check_correctness/check_correctness.py
@@ -1040,35 +1040,3 @@ if __name__ == "__main__":
 
     curations = args[0]
     statement_jsons_file = args[1]
-    #main(curations_file=curations, statements_file=statement_jsons_file)
-
-# curs_sample = random.sample(curs, 100)
-# responses = []
-#
-# for cur in tqdm.tqdm(curs_sample):
-#     stmt = stmts_by_hash[cur["pa_hash"]]
-#     ev = [e for e in stmt.evidence if e.get_source_hash() == cur["source_hash"]][0]
-#     eng_stmt = EnglishAssembler([stmt]).make_model()
-#     response = openai.Completion.create(
-#         model="text-davinci-003",
-#         prompt=prompt % (ev.text, eng_stmt),
-#         temperature=0,
-#         max_tokens=1,
-#         top_p=1.0,
-#         frequency_penalty=0.0,
-#         presence_penalty=0.0,
-#     )
-#     responses.append(response)
-#
-# choices = [r["choices"][0]["text"].strip() for r in responses]
-# tags = [c["tag"] for c in curs_sample]
-# confusion = defaultdict(int)
-# for choice, tag in zip(choices, tags):
-#     if choice == "Yes" and tag == "correct":
-#         confusion[("Yes", "correct")] += 1
-#     elif choice == "Yes" and tag != "correct":
-#         confusion[("Yes", "incorrect")] += 1
-#     elif choice == "No" and tag == "correct":
-#         confusion[("No", "correct")] += 1
-#     elif choice == "No" and tag != "correct":
-#         confusion[("No", "incorrect")] += 1
