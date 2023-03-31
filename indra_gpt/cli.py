88506fcee0b6e5b584537f5737f8fac6f468aacd
kkaris <karis.klas@gmail.com>
2023-03-31 14:54:17 -0700

    Add options to cli. Add defaults to help string.

--- indra_gpt/cli.py
+++ indra_gpt/cli.py
@@ -7,7 +7,8 @@ requires an API key to openai).
 
 import click
 
-from indra_gpt.check_correctness import get_create_training_set, run_stats
+from indra_gpt.check_correctness import get_create_training_set, run_stats \
+    as run_stats_func
 
 
 @click.group("indra-gpt")
@@ -46,40 +47,56 @@ def create_training_data(curations_file: str, statements_file: str, force: bool)
     "--run-iter",
     type=int,
     default=100,
-    help="The number of queries to send to chat-gpt."
+    help="The number of queries to send to chat-gpt. Default: 100."
 )
 @click.option(
     "--pos-examples",
     type=int,
     default=2,
-    help="The number of positive examples to use in the prompt."
+    help="The number of positive examples to use in the prompt. Default: 2."
 )
 @click.option(
     "--neg-examples",
     type=int,
     default=2,
-    help="The number of negative examples to use in the prompt."
+    help="The number of negative examples to use in the prompt. Default: 2."
+)
+@click.option(
+    "--mak-tokens",
+    type=int,
+    default=2,
+    help="The maximum number of tokens to use for the responses. Default: 2."
 )
 @click.option(
     "--debug-print",
     is_flag=True,
     help="Print the prompt sent and the full response from chat-gpt API call."
 )
-def run_stats_cli(
+@click.option(
+    "--file-title",
+    type=str,
+    default="",
+    help="The title to use for the output file."
+)
+def run_stats(
     run_iter: int,
     pos_examples: int,
     neg_examples: int,
-    debug_print: bool
+    mak_tokens: int,
+    debug_print: bool,
+    file_title: str,
 ):
     """Run the stats command line tool."""
     df = get_create_training_set(refresh=False)
 
-    _ = run_stats(
+    _ = run_stats_func(
         training_data_df=df,
         n_iter=run_iter,
         n_pos_examples=pos_examples,
         n_neg_examples=neg_examples,
-        debug_print=debug_print
+        max_tokens=mak_tokens,
+        debug_print=debug_print,
+        file_title=file_title or None,
     )
 
 
