eb3f7298cd4bbb21f9172dd40f9b06d180339a5c
Bihan Dasgupta <bihand2005@gmail.com>
2023-09-18 14:09:40 -0400

    Organized Code And Fixed Comments

--- indra_gpt/run_statement_json_extraction.py
+++ indra_gpt/run_statement_json_extraction.py
@@ -1,20 +1,16 @@
-"""This module contains functions for extracting statements from text
-feeding the full json schema using ChatGPT."""
+"""This module contains functions for extracting statements from text by
+feeding the full json schema to ChatGPT."""
 
+# import libraries
 import json
 import random
-
 import pandas as pd
 from indra.statements.io import stmt_from_json
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
-
 from api import run_openai_chat
 from constants import JSON_SCHEMA
-
 import openai.error
-
-
 
 def gpt_stmt_json(stmt_json_examples, evidence_text):
     """
@@ -24,15 +20,12 @@ def gpt_stmt_json(stmt_json_examples, evidence_text):
 
     :param stmt_json_examples:
     :param evidence_text:
-    :return:
+    :return response:
     """
+
     ############################# PROMPT ENGINEERING ###########################
 
     json_schema_string = json.dumps(JSON_SCHEMA)  # converting json schema to a string
-    # print(len(json))
-
-
-
 
     # full prompt including schema
     PROMPT = (
@@ -73,7 +66,7 @@ def gpt_stmt_json(stmt_json_examples, evidence_text):
     # the first example json to feed chatGPT as a sample, the reduced prompt
     # without schema, the second example json to feed chatGPT as a sample
     history = [
-        {"role": "user", "content": PROMPT + ev_text_1},  # Prompt with schema
+        {"role": "user", "content": PROMPT + ev_text_1},  # prompt with schema
         {"role": "assistant", "content": json.dumps(stmt_json_1)},
         # first stmt json example
         {
@@ -104,8 +97,14 @@ def gpt_stmt_json(stmt_json_examples, evidence_text):
     return chat_gpt_json  # return chatGPT's response
 
 def process_indra_object(stmt):
-    """get rid of irrelevant parts of the indra statement dictionary"""
-    #evidence = stmt["evidence"]
+    """
+    function to get rid of irrelevant parts of the indra statement
+    dictionary
+
+    :param stmt:
+    :return stmt:
+
+    """
 
     stmt["evidence"] = [{"text": stmt["evidence"][0].get("text")}]
 
@@ -119,67 +118,78 @@ def process_indra_object(stmt):
 
     return stmt
 
-# main function to run on the inputted training dataframe of json objects
 def main(json_file):
-    # main function on
-    # path to training dataset of sample json objects (change
-    # path as needed)
+
+    """
+    function to run above operations on inputted training dataframe of
+    json objects
+
+    :param json_file:
+    :output tsv file:
+
+    """
+
     outputs = []  # list of every output by chatGPT
     sentences = []  # list of the sentences fed to the prompt
-
     statements = []  # list of json statements returned from outputted json
     # object by chatGPT (when applicable to a sentence and its outputted json
     # object)
 
     with open(json_file, "r") as f:
-        json_content = json.load(f)
+        json_content = json.load(f) # load json file
 
-    json_object_list = json_content[:50]  # append entire file of sample
+    json_object_list = json_content[:50]  # append first 50 json objects to
+    # json_object_list
     json_object_list = [process_indra_object(stmt) for stmt in
-    json_object_list]
-    # json
-    # objects to the list json_object_list
+    json_object_list] # run processing function on each json object to trim
+    # it down
 
     for json_object in tqdm(
         json_object_list, desc="Extracting", unit="statement", unit_scale=True
-    ):
+    ): # loop through each json object in json_object_list
+
         #sample_list = [json_object_list[0],json_object_list[1]] # unhash
         # when debugging
-        sample_list = random.sample(json_object_list, 2) # make it
-        # deterministic
-        # just take the elements until testing is done then make it random
-        # this is important because we want to understand the input while
-        # we're testing.
+
+        sample_list = random.sample(json_object_list, 2) # take in a random
+        # sample of any 2 json objects to feed to the chat history per
+        # iteration
 
         sentence = json_object["evidence"][0]["text"]
-        sentences.append(sentence)
+        sentences.append(sentence) # get the sentence from the current json
+        # object and append to the sentences list
 
         try:
             with logging_redirect_tqdm():
-                response = gpt_stmt_json(sample_list, json_object)
+                response = gpt_stmt_json(sample_list, json_object) # run
+                # gpt_stmt_json function on the randomly sampled list and
+                # current json object
         except openai.error.InvalidRequestError as e:
-            error_text = f"OpenAI error: {e}"
-            outputs.append(error_text)
-            statements.append(error_text)
+            error_text = f"OpenAI error: {e}" # if chatGPT fails to create a
+            # response
+            outputs.append(error_text) # append error message
+            statements.append(error_text) # append error message
             continue
 
-        outputs.append(response)
+        outputs.append(response) # append chatGPT's response to outputs list
 
-        # Some generated json objects are able to take in the
-        # json.loads and stmt_from_json functions to return a statement
-        # json. Some are not. Use the try/except method to extract statement
-        # jsons from the generated json objects for the ones that work.
-        # Append them to the list of all statements extracted. If it doesn't
-        # work on a generated json object, just append that there was an
-        # error loading the statement.
-
+        """
+        Some generated json objects are able to take in the
+        json.loads and stmt_from_json functions to return a statement
+        json. Some are not. Use the try/except method to extract statement
+        jsons from the generated json objects for the ones that work.
+        Append them to the list of all statements extracted. If it doesn't
+        work on a generated json object, just append that there was an
+        error loading the statement.
+        """
         try:
-            json_str = json.loads(response)
-            stmt_n = stmt_from_json(json_str)  # INDRA statement object
-
-            statements.append(stmt_n)
+            json_str = json.loads(response) # load the response
+            stmt_n = stmt_from_json(json_str)  # extract the INDRA statement
+            # object from the string
+            statements.append(stmt_n) # append extracted statement to
+            # statements list
         except IndexError as e:
-            statements.append(f"Error: {e}")
+            statements.append(f"Error: {e}") # otherwise append error message
 
     df = pd.DataFrame(
         {
@@ -188,16 +198,12 @@ def main(json_file):
             "generated_json_object": outputs,
             "extracted_statement": statements,
         }
-    )
+    )     # save sentences, json_object_list, outputs, and statements as a pandas
+    # dataframe
     df.to_csv("statement_json_extraction_results.tsv", sep="\t",
-              index=False)
+              index=False) # save dataframe as tsv file
 
-    print("First five json objects generated by chatGPT: \n\n" + str(
-        outputs[:5]))  #
-    # only print the first 5 results
-    print("\n\nThe actual json objects: \n\n" + str(json_object_list[:5]))
     print("Done.")  # print done to know when main function has finished
 
-
 if __name__ == "__main__":
-    main("indra_benchmark_corpus_all_correct.json")
+    main("indra_benchmark_corpus_all_correct.json") # run main function here
